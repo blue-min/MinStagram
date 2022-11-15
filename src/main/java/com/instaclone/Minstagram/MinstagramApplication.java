79fee16cdb3bf61f515d2558c6bb30a0bedbd741
DESKTOP-10M8JTR\정민 <ilb0919497@gmail.com>
2022-11-15 17:27:09 +0900

    스프링 부트 테스트 코드 작성 및 포트 8090으로 변경

--- src/main/java/com/instaclone/Minstagram/MinstagramApplication.java
+++ src/main/java/com/instaclone/Minstagram/MinstagramApplication.java
@@ -2,12 +2,30 @@ package com.instaclone.Minstagram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+//test ------------------------------------------------------------
+@RestController
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class MinstagramApplication {
 
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(MinstagramApplication.class, args);
 	}
-
 }
+// test -------------------------------------------------------------
+//
+//@SpringBootApplication
+//public class MinstagramApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(MinstagramApplication.class, args);
+//	}
+//
+//}
